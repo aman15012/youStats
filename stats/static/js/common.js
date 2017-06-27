2ffb378ec1cc518fa60845afe75db261e2363824
Aman Agarwal <aman15012@iiitd.ac.in>
2017-06-27 05:45:01 +0530

    Update common.js

--- stats/static/js/common.js
+++ stats/static/js/common.js
@@ -1,3 +1,3 @@
 // Called automatically when JavaScript client library is loaded.
-var key = 'AIzaSyANEX5jkO4_t1soIt-6Dgek58Iz2px86lg'; //API key
+var key = 'YOUR-API-KEY-HERE'; //API key
 var query = "photography"; // the query
